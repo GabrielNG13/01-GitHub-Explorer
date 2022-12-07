834efc9acebccf2e6d97a4004585e74bb1da7b60
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2022-12-06 22:00:10 -0300

    React config

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
-//Não precisou indicar a extensão .jsx devido o resolve do webpack
+
+render(<App/>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,16 @@
-//Exportando o caminho raiz do projeto
 const path = require('path')
 
 module.exports = {
-    //Usando require para acessar o arquivo ao invés de src/index.js
-    //Evita problemas de incompatibilidade de / ou \ entre SOs
+    mode: 'development',
     entry: path.resolve(__dirname,'src','index.jsx'),
-    //Define o arquivo de saída
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
     },
-    //Identifica as extensões que podem ser lidas
     resolve: {
         extensions: ['.js', '.jsx']
     },
-    //Descreve como a aplicação vai se comportar quando for
-    //efetuada a importação de cada tipo de arquivo
     module: {
-        //Regras contendo um objeto para cada tipo de arquivo
-        //Verificações através de expressões regulares
         rules: [
             {
                 test: /\.jsx$/,
